edf81dba14e6a351293031e42435643cd37532b7
Poarse <zhangyida2017@gmail.com>
2020-09-28 22:01:20 +0800

    2020.9.28

---
+++ .history/常用算法/遍历算法_20200915224633.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+//便利算法
+void print01(int val)
+{
+      cout <<val << " "<< endl;
+}
+void test01()
+{
+
+      vector<int>v;
+      for (int i = 0; i < 10; i++)
+      {
+            /* code */
+            v.push_back(i);
+            
+     }
+
+      for_each(v.begin(), v.end(),print01);
+}
+
+int main()
+{
+      test01();
+      system("pause");
+      return 0;
+}
